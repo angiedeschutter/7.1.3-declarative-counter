c11d14f32663c857eccf0f5e0a25ede931f5a976
Angiegit config --global user.name 'Angie <adeschutter6437@gmail.com>
2023-05-11 19:05:31 -0700

    set react app

---
+++ babelProject/lib/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var multiply = function multiply(a, b) {
+  return a * b;
+};
+var firstNum = 53;
+var secondNum = 99;
+var product = multiply(firstNum, secondNum);
+console.log("the product of ".concat(firstNum, " and ").concat(secondNum, " is ").concat(product));
---
+++ babelProject/src/index.js
@@ -0,0 +1,7 @@
+const multiply = (a,b)=> a*b
+const firstNum = 53
+const secondNum= 99
+
+let product = multiply(firstNum, secondNum)
+
+console.log(`the product of ${firstNum} and ${secondNum} is ${product}`)
